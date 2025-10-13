9bfa42fc1e7c38f6434f8d9a6d2a62efcb7de61b
humanmilkbank9000-art <humanmilkbank9000@gmail.com>
2025-10-14 00:57:37 +0800

    Update user-login.blade.php

--- resources/views/user-login.blade.php
+++ resources/views/user-login.blade.php
@@ -677,6 +677,9 @@
             <p class="app-subtitle">
                 Please log in to access your account or to register, Hello World.
             </p>
+            <p class="app-subtitle">
+                Version 3.
+            </p>
         </div>
 
         <!-- Right Panel - Login Form -->
