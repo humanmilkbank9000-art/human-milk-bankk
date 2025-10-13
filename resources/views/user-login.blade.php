38b11cd46b149838205f88e561c8ae6cc3fbb399
humanmilkbank9000-art <mattrich621@gmail.com>
2025-10-13 22:50:30 +0800

    update login

--- resources/views/user-login.blade.php
+++ resources/views/user-login.blade.php
@@ -678,7 +678,7 @@
                 Please Log in to access your account or to register if you have not.
             </p>
             <p class="app-subtitle">
-                Version 1.0.0.0
+                Version 2.0.0.0
             </p>
         </div>
 
