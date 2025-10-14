c0e3abbfb71959135e3d2bae86e14b864f7505e9
humanmilkbank9000-art <mattrich621@gmail.com>
2025-10-14 12:59:21 +0800

    sqqq

--- resources/views/user-register.blade.php
+++ resources/views/user-register.blade.php
@@ -539,7 +539,7 @@
                         // Show/hide requirements
                         if (contactInput.value.length === 0) {
                             reqMsg.style.display = 'none';
-                        } else if (contactInput.value.length >= 12) {
+                        } else if (!/^\d{11}$/.test(contactInput.value)) {
                             reqMsg.style.display = 'block';
                         } else {
                             reqMsg.style.display = 'none';
